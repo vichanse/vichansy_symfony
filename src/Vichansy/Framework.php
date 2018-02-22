8f3d411460e8b0792dcd64d0aa7960d11502b9be
Vicky <vicky.nseng@gmail.com>
2018-02-22 06:42:31 +0100

    Dependency injection

--- src/Vichansy/Framework.php
+++ src/Vichansy/Framework.php
@@ -8,66 +8,8 @@
 
 namespace Vichansy;
 
-use Symfony\Component\EventDispatcher\EventDispatcher;
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\RequestStack;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Controller\ArgumentResolverInterface;
-use Symfony\Component\HttpKernel\Controller\ControllerResolverInterface;
-use Symfony\Component\Routing\Exception\ResourceNotFoundException;
-use Symfony\Component\Routing\Matcher\UrlMatcherInterface;
-
-
 use Symfony\Component\HttpKernel\HttpKernel;
 
 class Framework extends HttpKernel
 {
-    protected $dispatcher;
-    protected $resolver;
-    protected $requestStack;
-    private $argumentResolver;
-
-    public function __construct(
-        EventDispatcherInterface $dispatcher,
-        ControllerResolverInterface $resolver,
-        RequestStack $requestStack,
-        ArgumentResolverInterface $argumentResolver)
-    {
-        $this->dispatcher = $dispatcher;
-        $this->resolver = $resolver;
-        $this->requestStack = $requestStack;
-        $this->argumentResolver = $argumentResolver;
-        parent::__construct($dispatcher, $resolver, $requestStack, $argumentResolver);
-
-
-    }
-
-    public function handle(
-        Request $request,
-        $type = HttpKernelInterface::MASTER_REQUEST,
-        $catch = true
-    )
-    {
-        $this->matcher->getContext()->fromRequest($request);
-
-        try {
-            $request->attributes->add($this->matcher->match($request->getPathInfo()));
-
-            $controller = $this->controllerResolver->getController($request);
-            $arguments = $this->argumentResolver->getArguments($request, $controller);
-
-            $response = call_user_func_array($controller, $arguments);
-        } catch (ResourceNotFoundException $e) {
-            $response = new Response('Not Found', 404);
-        } catch (\Exception $e) {
-            $response = new Response('An error occurred', 500);
-        }
-
-        // dispatch a response event
-
-        $this->dispatcher->dispatch('response', new ResponseEvent($response, $request));
-
-        return $response;
-    }
 }
--- web/front.php
+++ web/front.php
@@ -7,41 +7,13 @@
  */
 require_once __DIR__.'/../vendor/autoload.php';
 
-use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\RequestStack;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel;
-use Symfony\Component\Routing;
+
+$routes = include __DIR__.'/../src/app.php';
+$sc = include __DIR__.'/../src/container.php';
 
 $request = Request::createFromGlobals();
-$requestStack = new RequestStack();
-$routes = include __DIR__.'/../src/app.php';
 
-$context = new Routing\RequestContext();
-$matcher = new Routing\Matcher\UrlMatcher($routes, $context);
+$response = $sc->get('framework')->handle($request);
 
-$controllerResolver = new HttpKernel\Controller\ControllerResolver();
-$argumentResolver = new HttpKernel\Controller\ArgumentResolver();
-
-$dispatcher = new EventDispatcher();
-$dispatcher->addSubscriber(new HttpKernel\EventListener\RouterListener($matcher, $requestStack));
-
-$errorHandler = function (Symfony\Component\Debug\Exception\FlattenException $exception) {
-    $msg = 'Something went wrong! ('.$exception->getMessage().')';
-
-    return new Response($msg, $exception->getStatusCode());
-};
-$dispatcher->addSubscriber(new HttpKernel\EventListener\ExceptionListener($errorHandler));
-
-$listener = new HttpKernel\EventListener\ExceptionListener(
-    'Calendar\Controller\ErrorController::exceptionAction'
-);
-$dispatcher->addSubscriber($listener);
-
-$dispatcher->addSubscriber(new Vichansy\StringResponseListener());
-
-$framework = new Vichansy\Framework($dispatcher, $controllerResolver, $requestStack, $argumentResolver);
-
-$response = $framework->handle($request);
 $response->send();
